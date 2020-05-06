e0e70f16bbc5b95e3ab68082213407c78ed57d87
Yann Klein <yann.klein@me.com>
2020-05-06 11:00:40 +0900

    lecture done

--- src/index.js
+++ src/index.js
@@ -1,50 +1,56 @@
-// //////
+// //////////////////////
 // Rehearsal
-// //////
+// //////////////////////
 
 // // 1.Select the button
 // const button = document.querySelector("#click-me");
-
 // // 2. Listen to a click
 // button.addEventListener("click", (event) => {
-//   // 3. Change the DOM
+//   // 3. Change the DOM (add disabled class and change innerText to loading)
 //   event.currentTarget.classList.add("disabled");
 //   event.currentTarget.innerText = "Loading...";
 // });
 
-// 1. Select elements
-const input = document.querySelector("#keyword");
-const button = document.querySelector("#submit");
-const list = document.querySelector("#results");
 
-const fetchAPI = (keyword) => {
+
+// //////////////////////
+// HTTP GET request
+// //////////////////////
+
+// 1. Select elements
+const input = document.querySelector('#keyword');
+const button = document.querySelector('#submit');
+const list = document.querySelector('#results');
+
+const fetchMovie = (keyword) => {
   const url = `http://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`;
   fetch(url)
     .then(response => response.json())
     .then((data) => {
-      // console.log(data.Search[0].Title);
+      console.log(data.Search[0]);
       // 3. Change the DOM
-      list.innerHTML = "";
       data.Search.forEach((movie) => {
-        list.insertAdjacentHTML("beforeend", `
-          <li class='list-inline-item'>
-            <img src="${movie.Poster}"/>
-            <p>${movie.Title}</p>
-          </li>
-        `);
+        list.insertAdjacentHTML(
+          "beforeend",
+          `<li list-inline-item><img src="${movie.Poster}"><p>${movie.Title}</p></li>`
+        );
       });
     });
 };
 
 // 2. Listen to a click
 button.addEventListener("click", (event) => {
-  // 2.5. Fetch the API
   event.preventDefault();
-  fetchAPI(input.value);
+  list.innerHTML = "";
+  // 2.5. Fetch the API
+  fetchMovie(input.value);
 });
 
 
+
+// //////////////////////
 // HTTP POST request
+// //////////////////////
 
 const searchAlgoliaPlaces = (event) => {
   // 2.5 Fetch API
@@ -61,5 +67,4 @@ const searchAlgoliaPlaces = (event) => {
 // 1. Select an element
 const search = document.querySelector("#search");
 // 2. Add event listener
-search.addEventListener("keyup", searchAlgoliaPlaces);
-
+input.addEventListener("keyup", searchAlgoliaPlaces);
